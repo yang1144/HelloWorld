b6ddc37bedfd3e2e881560610519841a8f04e4ac
yang1144 <xing2080@qq.com>
2018-11-15 17:28:10 +0800

    xiugai  33

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World haha lalala sss  ff!");
+        System.out.println("Hello World haha lalala sss  ff ss!");
     }
 }
